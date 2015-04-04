244de59d9e0cc9582849207e7b2438827c813bcd
Eduardo Araya Castro <eduardoarayacastro@MacBook-Pro-de-Eduardo.local>
2015-04-04 13:44:46 -0300

    Arreglos parte dos

---
+++ arreglo_dos.py
@@ -0,0 +1,21 @@
+frutas = ["Manzana","Mango","Uva","Sandia"]
+
+#append -> Agregar un elemento al arreglo
+frutas.append("Fresas")
+
+#inser -> Agregar 2 instrucciones, una el elemento y posicion
+frutas.insert(2,"Naranja")
+
+#extend -> agregar los elementos de una arreglo a otro arreglo
+verduras = ["Lechuga", "Papa", "Zapallo"]
+frutas.extend(verduras)
+
+#remove -> remover el elemento del arreglo indicado
+frutas.remove("Manzana")
+
+#pop -> remover el elemento segun su posicion
+frutas.pop(5)
+
+
+
+print frutas
